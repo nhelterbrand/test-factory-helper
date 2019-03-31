e30b8b7fa98c74a0824b59975b9c6c39a742f5d7
Nate Helterbrand <nhelterbrand@wmp.com>
2019-03-30 20:33:13 -0500

    updating scratch-org script
    to be able to use for any folder

--- scratch-org.sh
+++ scratch-org.sh
@@ -2,9 +2,11 @@
 # @author Nate Helterbrand
 # helpful bash script to get started with this repo's code quickly
 
+folderName=${PWD##*/}
+
 # create scratch org
-echo 'Creating Scratch Org and setting Scratch Org as Default Username (for VSCode SFDX extension use)...'
-sfdx force:org:create -f config/project-scratch-def.json -a TestFactoryScratch --setdefaultusername
+echo 'Creating Scratch Org for '"$folderName"' and setting Scratch Org as Default Username (for VSCode SFDX extension use)...'
+sfdx force:org:create -f config/project-scratch-def.json -a "$folderName"-scratch --setdefaultusername
 
 # push metadata from package to scratch org
 echo 'Pushing metadata to the newly created Scratch Org, because otherwise you will forget...'
@@ -12,6 +14,6 @@ sfdx force:source:push
 
 # open scratch org
 echo 'Opening Scratch Org, so close!...'
-sfdx force:org:open -u TestFactoryScratch
+sfdx force:org:open -u "$folderName"-scratch
 
 echo 'Done! Thanks for checking this out!'
